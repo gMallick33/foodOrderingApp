dc0842325e613fc1405a85d6eebfbab844640884
gyani <g.arya.mallick@gmail.com>
2023-10-25 16:45:36 +0530

    implemented 'lazy loading' for about page

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
-import About from './components/About';
+// import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import RestaurantMenu from './components/RestaurantMenu';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
     return (
@@ -29,7 +32,7 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />
+        element: <Suspense fallback={<h1>About is loading suspensefully...</h1>}><About /></Suspense>
       },
       {
         path: "/contact",
